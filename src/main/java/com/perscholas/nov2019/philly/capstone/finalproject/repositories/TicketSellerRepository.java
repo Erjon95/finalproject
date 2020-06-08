8040442c59ef58874a50ece224489ca59bc44157
Erjon Dervishaj <er_djs@yahoo.com>
2020-06-08 00:02:40 -0400

    working!

--- src/main/java/com/perscholas/nov2019/philly/capstone/finalproject/repositories/TicketSellerRepository.java
+++ src/main/java/com/perscholas/nov2019/philly/capstone/finalproject/repositories/TicketSellerRepository.java
@@ -46,7 +46,7 @@ public interface TicketSellerRepository extends CrudRepository<TicketSeller, Int
 
     @Transactional
     @Modifying
-    @Query(value = "insert into ticketseller(orgname, orgaddress, webaddress, contactfirstname, contactlastname, contactemail, contactphone, password) values(:orgname, :orgaddress, :webaddress, :contactfirstname, :contactlastname, :contactemail, :contactphone, :password)", nativeQuery = true)
+    @Query(value = "insert into ticket_seller(orgname, orgaddress, webaddress, contactfirstname, contactlastname, contactemail, contactphone, password) values(:orgname, :orgaddress, :webaddress, :contactfirstname, :contactlastname, :contactemail, :contactphone, :password)", nativeQuery = true)
     void registerSeller(@Param("orgname") String orgname, @Param("orgaddress") String orgaddress, @Param("webaddress") String webaddress, @Param("contactfirstname") String contactfirstname, @Param("contactlastname") String contactlastname, @Param("contactemail") String contactemail, @Param("contactphone") String contactphone, @Param("password") String password);
 
     @Transactional
